e8217c8db9a66b5b9e6bdf5948b8084f9cb50a51
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-09-01 02:57:01 -0700

    update the type of user avatar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 
 export const Card = props => (
     <div className='card-container'>
-        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=175x175`} alt="monster mugshot"/>
+        <img src={`https://robohash.org/${props.monster.id}?set=set3&size=175x175`} alt="monster mugshot"/>
         <h2> {props.monster.name} </h2>
         <p> {props.monster.email} </p>
     </div>
