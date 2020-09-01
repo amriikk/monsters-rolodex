622ed75b8d3b4233f21aaf15b6a596f36146a962
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-09-01 01:53:38 -0700

    update Card.css file

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Card.css'
+
+export const Card = props => (
+    <div className='card-container'>
+        <img src={`https://robohash.org/${props.monster.id}?set=set2&size=175x175`} alt="monster mugshot"/>
+        <h2> {props.monster.name} </h2>
+        <p> {props.monster.email} </p>
+    </div>
+)
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Card } from '../Card/Card'
 import './CardList.css'
 
 export const CardList = props => (
     <div className='CardList'>
         {props.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name} </h1>
+            <Card key={monster.id} monster={monster} />      
         ))}
     </div>
 );
-    
