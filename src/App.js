d56664df0e888ff0b634ef8c3b2fceb59f726fc2
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-09-01 01:23:53 -0700

    update CardList component

--- src/App.js
+++ src/App.js
@@ -14,18 +14,13 @@ class App extends Component {
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monster: users}));
+      .then(users => this.setState({monsters: users}));
   }
 
   render() {
     return (
       <div className="App">
-        <CardList />
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name} </h1>
-          ))
-        }
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }      
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import './CardList.css'
 
-export const CardList = props => {
-    console.log(props);
-    return <div>Hello</div>;
-};
+export const CardList = props => (
+    <div className='CardList'>
+        {props.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+        ))}
+    </div>
+);
+    
