94c4a0b161e4846af76d7748c6d579259458d99b
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-09-03 02:03:31 -0700

    finished up CSS styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {CardList} from './components/CardList/CardList.jsx'
+import { CardList } from './components/CardList/CardList.jsx'
+import { SearchBox } from './components/SearchBox/SearchBox.jsx'
 import './App.css';
 
 class App extends Component {
@@ -7,20 +8,37 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
+
+    // this.handleChange = this.handleChange.bind(this); // binds this to the method we created
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monsters: users}));
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={ this.handleChange } 
+        />
+        <CardList monsters={ filteredMonsters } />
       </div>
     );
   }      
