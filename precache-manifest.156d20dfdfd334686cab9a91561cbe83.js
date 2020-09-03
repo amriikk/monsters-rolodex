180ed03803ba2c57f3137a561fcb62d2d7fdd81b
John Khrizhtian Trujillo <yiannix@johns-mbp.home>
2020-09-03 02:09:57 -0700

    Updates

---
+++ precache-manifest.156d20dfdfd334686cab9a91561cbe83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696d71c38962ba1f09f0362f3f6ef299",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e0f7e90da114b5dd4508",
+    "url": "/monsters-rolodex/static/css/main.08779b76.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f7e90da114b5dd4508",
+    "url": "/monsters-rolodex/static/js/main.c350b670.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
